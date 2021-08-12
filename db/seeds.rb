677ca8b686ae520aa6a784ae354c20c04f9f914b
Jonathan Kerbrat <jonathan.kerbrat@hotmail.com>
2021-08-12 14:13:09 +0200

    mep ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times { Movie.create(title: Faker::Movie.title, overview: Faker::Movie.quote, poster_url: Faker::Internet.url, rating: Faker::Number.between(from: 1, to: 10))}
