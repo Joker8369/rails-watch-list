ddc8600d0e65cd82381db2ed2334659de2a434b2
Jonathan Kerbrat <jonathan.kerbrat@hotmail.com>
2021-08-13 16:25:56 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,11 +14,11 @@
 
 # #  JSON PARSE
 
-# url = "http://tmdb.lewagon.com/movie/top_rated"
-# api_content_json = URI.open(url).read
-# data = JSON.parse(api_content_json)
-# movies_results_array = data["results"] # array of movies hashes
+url = "http://tmdb.lewagon.com/movie/top_rated"
+api_content_json = URI.open(url).read
+data = JSON.parse(api_content_json)
+movies_results_array = data["results"] # array of movies hashes
 
-# movies_results_array.each do |movie|
-#   Movie.create(title: movie["title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}", rating: movie["vote_average"])
-# end
+movies_results_array.each do |movie|
+  Movie.create(title: movie["title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}", rating: movie["vote_average"])
+end
