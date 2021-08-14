14ce86cdb1ea3e4f4bce990401ddb712cd69efad
jonathan kerbrat <jonathan.kerbrat@hotmail.com>
2021-08-14 10:36:32 +0200

    test deploiement

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :show, :new, :create, :destroy] do
+  resources :lists do
     resources :bookmarks, only: [ :new, :create]
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,20 +3,20 @@
 #
 # Examples:
 
-# List.destroy_all
+List.destroy_all
 
-# #
-# require 'json'
-# require 'open-uri'
+#
+require 'json'
+require 'open-uri'
 
 
-# #  JSON PARSE
+#  JSON PARSE
 
-# url = "http://tmdb.lewagon.com/movie/top_rated"
-# api_content_json = URI.open(url).read
-# data = JSON.parse(api_content_json)
-# movies_results_array = data["results"] # array of movies hashes
+url = "http://tmdb.lewagon.com/movie/top_rated"
+api_content_json = URI.open(url).read
+data = JSON.parse(api_content_json)
+movies_results_array = data["results"] # array of movies hashes
 
-# movies_results_array.each do |movie|
-#   Movie.create(title: movie["title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}", rating: movie["vote_average"])
-# end
+movies_results_array.each do |movie|
+  Movie.create(title: movie["title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/w500/#{movie["poster_path"]}", rating: movie["vote_average"])
+end
